73f517159494e5e6c59beba6eedb2279c60546cd
lanichlanich <lanichlanich@gmail.com>
2020-05-29 15:31:32 +0700

    add author info and created date

--- app/User.php
+++ app/User.php
@@ -43,5 +43,11 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
+    public function getUrlAttribute()
+    {
+//        return route("questions.show", $this->id);
+        return "#";
+    }
+
 
 }
